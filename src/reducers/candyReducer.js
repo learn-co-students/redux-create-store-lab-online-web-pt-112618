d9936c9d250ce32b61b6b439373b3ea49daca204
yukijina <yukijina@gmail.com>
2019-09-10 19:13:38 -0700

    Done.

--- src/reducers/candyReducer.js
+++ src/reducers/candyReducer.js
@@ -2,6 +2,9 @@ function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
       return [...state, action.candy];
+    case 'REMOVE_CANDY':
+      state.pop
+      return state
     default:
       return state;
   }
